8e2faf0bf4d09804c3821a5c5f14cbdddbe8de48
shshhan <seunghyeonhan95@gmail.com>
2022-02-24 20:22:19 +0900

    finish mjg

--- 23_typeScript/23.1_typeScript2.ts
+++ 23_typeScript/23.1_typeScript2.ts
@@ -23,3 +23,47 @@ interface Benz extends Car {    //interface extends
     logo: string;
     modelNumber: string;
 }
+
+
+//class
+//prototype 상속을 수행하여 애플리케이션에서 재사용할수 있는 구성요소를 만들 수 있다
+//ES6의 클래스 : 모든 클래스 멤버는 공개
+//typescript의 클래스 : 클래스 멤버에 접근하는 권한을 설정 가능
+class Animal {
+    public eat = () => {
+        console.log('nyam nyam');
+    };
+    sleep = () => {
+        console.log('zzzz'); 
+    };
+}
+
+class Human extends Animal {
+    private work = () => {
+        console.log('fighting');
+    };
+
+    protected watchMovie = () => {
+        console.log('LaLa Land');
+    };
+}
+
+class Adult extends Human {
+    public listenToMusic = () => {
+        console.log('Rock music');
+    };
+
+    public goToTheater = () => {
+        console.log('I will watch ');
+        this.watchMovie();
+    }
+}
+
+const me = new Human();
+me.eat();
+me.sleep();
+// me.work();  //Property 'work' is private and only accessible within class 'Human'.  
+//me.watchMovie();    //Property 'watchMovie' is protected and only accessible within class 'Human' and its subclasses.
+
+const mom = new Adult();
+mom.goToTheater();
